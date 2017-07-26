668d1d36a1f2a15fd6423d1147b3a70a2a1e6fe7
AidarNugmanoff <a.nugmanoff@gmail.com>
2017-07-26 15:38:09 +0600

    feat: add get method for readme

--- server.rb
+++ server.rb
@@ -1,7 +1,13 @@
 # server.rb
-require 'yaml'
+require 'net/http'
 require 'bundler'
 Bundler.require
+
+def get_readme(owner, repo)
+  url = URI("https://api.github.com/repos/#{owner}/#{repo}/readme")
+  res = Net::HTTP.get_response(url) # => String
+  res.body
+end
 
 before do
   content_type 'application/json'
@@ -13,12 +19,14 @@ end
 
 get '/repos/' do
   lang = params[:language] if params[:language]
-  since = params[:since] if params[:language]
+  since = params[:since] if params[:since]
   repos = GitTrend.get(lang, since)
   repos.map { |repo|
     repo.to_h }.to_json
 end
 
 get '/repos/:owner/:repo/readme' do
-
+  owner = params[:owner] if params[:owner]
+  repo = params[:repo] if params[:repo]
+  get_readme(owner, repo)
 end
