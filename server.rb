ee552d1e5a28c4c2a2308c417be25a05f6028e81
AidarNugmanoff <a.nugmanoff@gmail.com>
2017-07-26 14:53:15 +0600

    feat: add languages.json, complete repos and languages calls

--- server.rb
+++ server.rb
@@ -3,20 +3,12 @@ require 'yaml'
 require 'bundler'
 Bundler.require
 
-def search_for_lang_color(lang)
-  yaml = YAML.load_file('languages.yml')
-  return yaml[lang]["color"]
-end
-
 before do
   content_type 'application/json'
 end
 
 get '/languages' do
-  response = {
-    color: search_for_lang_color("API Blueprint")
-  }
-  response.to_json
+  json = File.read('languages.json')
 end
 
 get '/repos/' do
